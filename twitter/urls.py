e16f6237ac19ab73c32ff06c90d0051d1c205d2d
yxguyxgu <yxguyxgu.gmail.com>
2023-01-24 22:38:23 -0800

    login, login status, signup, logout

--- twitter/urls.py
+++ twitter/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
 from accounts.api import views
+from accounts.api.views import AccountViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/users', views.UserViewSet)
+router.register(r'api/accounts', AccountViewSet, basename='accounts')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
